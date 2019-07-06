a56e196b632b06d5394a09bc9f5119f999712762
yhmane <hwang9yh@gmail.com>
2019-07-07 02:20:14 +0900

    about java singleton pattern

---
+++ src/singleton/SingletonMain.java
@@ -0,0 +1,19 @@
+package singleton;
+
+public class SingletonMain {
+    public static void main(String[] args) {
+        // Singleton의 생성자는 private이기 때문에 new로 인스턴스 생성 불가
+        //Singleton singleton1 = new Singleton();
+
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+        Singleton singleton3 = Singleton.getInstance();
+
+        singleton1.setInput("singleton1!!");
+
+        System.out.println(singleton1.getInput());
+        System.out.println(singleton2.getInput());
+        System.out.println(singleton3.getInput());
+
+    }
+}
