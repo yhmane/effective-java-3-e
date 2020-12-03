3aa27e9b5ddf323eba5212104927d6c3bcb8a651
hwangyunho <hwang9yh@gmail.com>
2020-12-04 00:07:14 +0900

    item4. 인스턴스화를 막으려거든 private 생성자를 사용하라

---
+++ src/chapter2/item4/Item4.java
@@ -0,0 +1,11 @@
+package chapter2.item4;
+
+public class Item4 {
+
+    private Item4() {
+    }
+
+    public static void play() {
+        System.out.println("just play");
+    }
+}
---
+++ src/chapter2/item4/Item4Main.java
@@ -0,0 +1,14 @@
+package chapter2.item4;
+
+import java.util.Arrays;
+
+public class Item4Main {
+
+    public static void main(String[] args) {
+        Math.abs(5);
+        Arrays.sort(new int[]{5, 3});
+
+        //Item4 item4 = new Item4();
+        Item4.play();
+    }
+}
